3f54f7b97890f4f9ff4cec9d17fdbd663a5d9410
Prome1heus <47352129+Prome1heus@users.noreply.github.com>
2019-02-20 19:48:38 +0100

    Add files via upload

--- checkpiece.cpp
+++ checkpiece.cpp
@@ -1,6 +1,19 @@
 #pragma once
 #include "util.cpp"
+#include <vector>
+
+using namespace std;
 
 bool checkSlice(vector<vector<int>> &pizza, int l, int h, Rect &r) {
-    return true;
+    if ((r.x1<0)||(r.y1<0)||(r.x2>=pizza.length)||(r.y2>=pizza[0].length)||(r.x1>r.x2)||(r.y1>r.r.y2)) return false;
+    int s = (r.x2-r.x1)*(r.y2-r.y1);
+    if (s>h) return false;
+    int c = 0;
+    for (int i=r.x1; i<=r.x2; i++){
+        for (int j=r.y1; j<=r.y2; j++){
+            if (pizza[i][j]==-1) return false;
+            if (pizza[i][j]==1) c++;
+        }
+    }
+    return (c>=l)&&((s-c)>=l);
 }
