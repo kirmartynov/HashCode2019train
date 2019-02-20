facad902c1efbd0f312db03d79db84ae7c23ddfc
Vladimir Fomenko <vlfomenk@gmail.com>
2019-02-20 20:33:33 +0100

    Fix solver

--- util.cpp
+++ util.cpp
@@ -21,7 +21,7 @@ struct Rect{
     int x1, y1, x2, y2;
 
     Rect() {}
-    Rect(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(y2), y2(y2) {
+    Rect(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {
     }
 };
 
