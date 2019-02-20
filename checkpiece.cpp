4bf211ff7239f1e847cb83356e193fe8b7c290d2
Vladimir Fomenko <vlfomenk@gmail.com>
2019-02-20 20:14:05 +0100

    Fix checkpiece

--- checkpiece.cpp
+++ checkpiece.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 bool checkSlice(vector<vector<int> > &pizza, int l, int h, Rect r) {
     if ((r.x1<0)||(r.y1<0)||(r.x2>=pizza.size())||(r.y2>=pizza[0].size())||(r.x1>r.x2)||(r.y1>r.y2)) return false;
-    int s = (r.x2-r.x1)*(r.y2-r.y1);
+    int s = (r.x2-r.x1+1)*(r.y2-r.y1+1);
     if (s>h) return false;
     int c = 0;
     for (int i=r.x1; i<=r.x2; i++){
